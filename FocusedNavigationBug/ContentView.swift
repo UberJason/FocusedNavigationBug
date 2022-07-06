5ed242fee61032897f608b06786462c8b6dfe1d7
Jason Ji <uberjason@gmail.com>
2022-07-06 19:45:06 -0400

    Initial Commit

---
+++ FocusedNavigationBug/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FocusedNavigationBug
+//
+//  Created by Jason Ji on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FocusedNavigationBug/FocusedNavigationBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusedNavigationBugApp.swift
+//  FocusedNavigationBug
+//
+//  Created by Jason Ji on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FocusedNavigationBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
