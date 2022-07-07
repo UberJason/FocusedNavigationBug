409a6432d4bb5950af284f27fa7cedb10c710251
Jason Ji <uberjason@gmail.com>
2022-07-06 20:25:01 -0400

    Initial commit with the project

--- FocusedNavigationBug/ContentView.swift
+++ FocusedNavigationBug/ContentView.swift
@@ -5,15 +5,80 @@
 //  Created by Jason Ji on 7/6/22.
 //
 
+import Combine
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowing = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Sheet") {
+            isShowing.toggle()
+        }
+        .sheet(isPresented: $isShowing) {
+            RootSheetView()
+        }
+    }
+}
+
+class Coordinator: ObservableObject {
+    @Published public var navigationStack = [Int]()
+    
+    public init() {}
+}
+
+struct RootSheetView: View {
+    @StateObject var coordinator = Coordinator()
+    
+    @FocusState private var focusedField: Int?
+    
+    var body: some View {
+        let _ = Self._printChanges()
+        NavigationStack(path: $coordinator.navigationStack) {
+            Form {
+                TextField("Recipe Name", text: Binding.constant(""))
+                    .focused($focusedField, equals: 1)
+
+                NavigationLink(value: 0) {
+                    Text("Manage Reminders")
+                }
+            }
+            .safeAreaInset(edge: .bottom) {
+                if focusedField == nil {
+                    Text("Hmm").padding()
+                }
+            }
+            .navigationTitle("First Sheet")
+            .navigationDestination(for: Int.self) { screen in
+                ManageRemindersView()
+            }
+        }
+    }
+}
+
+struct ManageRemindersView: View {
+    @State var isShowing = false
+    
+    var body: some View {
+        Button("Show") {
+            isShowing.toggle()
+        }
+        .sheet(isPresented: $isShowing) {
+            NavigationStack {
+                AddReminderView() {
+                    isShowing = false
+                }
+                .navigationTitle("Second Sheet")
+            }
+        }
+    }
+}
+
+struct AddReminderView: View {
+    let onReminderAdded: () -> ()
+    
+    var body: some View {
+        Button("Dismiss") {
+            onReminderAdded()
         }
     }
 }
